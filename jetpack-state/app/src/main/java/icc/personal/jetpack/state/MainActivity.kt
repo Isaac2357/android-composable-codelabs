49d0d46f408f63346e2c2e11f1cb2935f0e993c3
Isaac Cabrera <isaaccabrera31@gmail.com>
2021-09-24 13:29:56 -0500

    Initial commit Jetpack State Codelab

---
+++ jetpack-state/app/src/main/java/icc/personal/jetpack/state/MainActivity.kt
@@ -0,0 +1,38 @@
+package icc.personal.jetpack.state
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import icc.personal.jetpack.state.ui.theme.JetpackStateTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            JetpackStateTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(color = MaterialTheme.colors.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    JetpackStateTheme {
+        Greeting("Android")
+    }
+}
